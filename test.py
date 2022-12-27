b732bc0b924e9f3a11bad5a311054bfc382af89e
KJ2309 <59178643+KJ2309@users.noreply.github.com>
2022-12-27 14:50:45 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,64 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/kalpj/Downloads"              # Add the path of you "Downloads" folder.
+to_dir = "C:/Users/kalpj/Desktop" #Create "Document_Files" folder in your Desktop and update the path accordingly.
+
+
+dir_tree = {
+    "Document_Files": [ '.pdf', '.txt', ".doc" , ".docx"]
+}
+
+# Event Hanlder Class
+
+class FileMovementHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+
+        name, extension = os.path.splitext(event.src_path)
+       
+        time.sleep(1)
+
+        for key, value in dir_tree.items():
+            time.sleep(1)
+            if extension in value:
+                file_name = os.path.basename(event.src_path)
+                path1 = from_dir + "/" + file_name
+                path2 = to_dir + "/" + key
+                path3 = to_dir + "/" + key + "/" + file_name
+
+                if os.path.exists(path2):
+                    shutil.move(path1,path3)
+                else:
+                    os.makedirs(path2)
+                    shutil.move(path1, path3)
+
+
+            
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+# Start the Observer
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped!")
+    observer.stop()
+
